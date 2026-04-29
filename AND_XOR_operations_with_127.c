db4c588bf908a8b57df01fefc485da90d27589b7
Rekha978 <152130211+Rekha978@users.noreply.github.com>
2026-04-29 21:50:17 +0530

    Update AND_XOR_operations_with_127.c

--- AND_XOR_operations_with_127.c
+++ AND_XOR_operations_with_127.c
@@ -17,3 +17,17 @@ void main() {
 		printf("%c", ch);
 	}
 }
+
+char *str="Hello world";
+int length,i;
+char result;
+clrscr();
+length = strlen(str);
+for(i=0;i<length;i++) {
+result = str[i]^127;
+printf("%c",result); }
+for(i=0;i<length;i++) {
+result = str[i]&127;
+printf("%c",result); }
+getch();
+return 0; 
